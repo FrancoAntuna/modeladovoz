3f8c68fd6f4a1fa263c8dc7538f74dafcd515889
FrancoAntuna <franco.antuna.nob@gmail.com>
2024-02-26 14:09:13 -0300

    upload project

---
+++ instalador.bat
@@ -0,0 +1,25 @@
+@echo off
+setlocal
+
+REM Clonar el repositorio desde GitHub
+git clone https://github.com/FrancoAntuna/modelovoz.git "%~dp0project-main"
+
+REM Cambiar al directorio recién creado
+cd /d "%~dp0project-main"
+
+REM Ejecutar el script de descarga de archivos
+python download_files.py
+
+REM Instalar los requisitos del proyecto
+pip install -r "%~dp0project-main\requirements-safe.txt"
+
+REM Instalar pyngrok y tensorflow en la versión específica
+pip install pyngrok tensorflow==2.12.0
+
+REM Eliminar la carpeta sample_data
+rmdir /s /q "%~dp0project-main\sample_data"
+
+REM Crear la carpeta dataset
+mkdir "%~dp0dataset"
+
+endlocal
